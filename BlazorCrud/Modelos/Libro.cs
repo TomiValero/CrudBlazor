7b3ec5ae73afa5c18620cc12537aeed7a1e21f58
Tomas Valero <tomasvalero22@gmail.com>
2024-09-02 16:20:31 -0300

    Subo mi primer proyecto en Blazor Server

---
+++ BlazorCrud/Modelos/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorCrud.Modelos
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El titulo es obligatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El autor es obligatorio")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "La cantidad de paginas es obligatoria")]
+        public int Paginas { get; set; }
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        public int Precio { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ BlazorCrud/Repositorio/Repositorio.cs
@@ -0,0 +1,69 @@
+﻿using BlazorCrud.Data;
+using BlazorCrud.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Repositorio
+{
+    public class Repositorio : IRepositorio
+    {
+        private readonly ApplicationDbContext _contexto;
+
+        public Repositorio(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<Libro> ActualizarLibro(int libroId, Libro actualizarLibro)
+        {
+           var libroDesdeBD = await _contexto.libro.FindAsync(libroId);
+
+            libroDesdeBD.Titulo = actualizarLibro.Titulo;
+            libroDesdeBD.Descripcion = actualizarLibro.Descripcion;
+            libroDesdeBD.Autor = actualizarLibro.Autor;
+            libroDesdeBD.Paginas = actualizarLibro.Paginas;
+            libroDesdeBD.Precio = actualizarLibro.Precio;
+
+            await _contexto.SaveChangesAsync();
+
+            return libroDesdeBD;
+        }
+
+        public async Task<Libro> CrearLibro(Libro crearLibro)
+        {
+            if (crearLibro != null)
+            {
+                crearLibro.Fecha = DateTime.Now;
+                await _contexto.libro.AddAsync(crearLibro);
+                await _contexto.SaveChangesAsync();
+                return crearLibro;
+            }
+            else
+            {
+                return new Libro();
+            }
+        }
+
+        public async Task EliminarLibro(int libroId)
+        {
+            var libroDesdeBD = await _contexto.libro.FindAsync(libroId);
+            _contexto.Remove(libroDesdeBD);
+
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task<Libro> GetLibroId(int libroId)
+        {
+            var libroDesdeBD = await _contexto.libro.FindAsync(libroId);
+            if (libroDesdeBD == null)
+            {
+                return new Libro();
+            }
+            return libroDesdeBD;
+        }
+
+        public Task<List<Libro>> GetLibros()
+        {
+            return _contexto.libro.ToListAsync();
+        }
+    }
+}
